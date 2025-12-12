be80d8a38e80ff9d4bffc63639e06bed4742c2d9
disneyprincesss <leilanelim123@gmail.com>
2025-12-12 22:29:16 +0800

    fix stats progress bar

--- app/components/ui/progress.tsx
+++ app/components/ui/progress.tsx
@@ -12,18 +12,28 @@ function Progress({
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
+        "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full border border-zinc-800",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className={`h-full w-full flex-1 transition-all ${value ? (value < 127.5 ? "bg-red-600" : value < 200 ? "bg-yellow-400" : "bg-lime-600") : ""}`}
-        style={{ transform: `translateX(-${100 - ((value ? (value / 255) * 100 : 0))}%)` }}
+        className={`h-full w-full flex-1 transition-all ${
+          value
+            ? value < 127.5
+              ? "bg-red-600"
+              : value < 200
+              ? "bg-yellow-400"
+              : "bg-lime-600"
+            : ""
+        }`}
+        style={{
+          transform: `translateX(-${100 - (value ? (value / 255) * 100 : 0)}%)`,
+        }}
       />
     </ProgressPrimitive.Root>
-  )
+  );
 }
 
 export { Progress }
