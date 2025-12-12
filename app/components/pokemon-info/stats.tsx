9a078626e1170d8d987af0f48073c88672bf0b45
disneyprincesss <leilanelim123@gmail.com>
2025-12-12 23:26:54 +0800

    fix stats progress bar

--- app/components/pokemon-info/stats.tsx
+++ app/components/pokemon-info/stats.tsx
@@ -20,10 +20,10 @@ export default function Stats({
       : "h-68 sm:h-72 lg:h-98";
 
   return (
-    <div className="stats">
+    <div className="stats flex flex-col justify-center w-full sm:w-full">
       <h3>Stats</h3>
       <ScrollArea className={`${scrollHeight} w-full`}>
-        <ul className="flex flex-col gap-4 w-100 h-62 sm:h-68 text-xl sm:text-2xl sm:w-125 mt-3">
+        <ul className="flex flex-col gap-4 w-full h-62 sm:h-68 text-sm sm:text-xl sm:w-125 mt-3">
           {pokemon.stats.map((s) => {
             const statName = s.stat.name
               .split("-")
@@ -31,9 +31,9 @@ export default function Stats({
               .join("-");
 
             return (
-              <li key={s.stat.name} className="w-full flex items-center gap-4">
-                <span className="w-2/3">{statName}</span>
-                <div className="w-full flex items-center justify-between">
+              <li key={s.stat.name} className="w-full flex items-center sm:gap-4">
+                <span className="w-3/7 sm:w-2/3">{statName}</span>
+                <div className="w-2/4 sm:w-full flex items-center justify-between">
                   <Progress value={s.base_stat} className="w-5/6" />
                   <span>{s.base_stat}</span>
                 </div>
